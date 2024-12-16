fc46185592ef3eee2cd29946522bd8ef49341ca0
Vlad Banea <vlad.banea@vention.cc>
2024-12-16 10:30:40 -0500

    with OSC 8 links

--- get-my-issues.py
+++ get-my-issues.py
@@ -2,6 +2,10 @@ from dotenv import load_dotenv
 import os
 from jira import JIRA
 
+# Construct the hyperlink using OSC 8 escape sequences
+def get_hyperlink(url, text):
+    return f"\033]8;;{url}\033\\{text}\033]8;;\033\\"
+
 load_dotenv()
 
 JIRA_SERVER = os.getenv('JIRA_SERVER')
@@ -17,6 +21,12 @@ jql_query = 'assignee = currentUser() ORDER BY priority DESC'
 
 issues = jira.search_issues(jql_query)
 
+key_width = 15
+status_width = 15
+
 for issue in issues:
-    print(f"{issue.key}: {issue.fields.summary} (Status: {issue.fields.status.name})")
+    key = get_hyperlink(issue.permalink(), issue.key).ljust(key_width)
+    summary = issue.fields.summary
+    status = f"{issue.fields.status.name}".ljust(status_width)
+    print(f"{key}: {status} {summary}")
 
