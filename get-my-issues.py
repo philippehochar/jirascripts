6bdadc57e68f4754f0d9b918045f67662630435f
Philippe Hochar <philippe.hochar@vention.cc>
2024-12-23 15:08:01 -0500

    Order by status name

--- get-my-issues.py
+++ get-my-issues.py
@@ -6,21 +6,39 @@ from jira import JIRA
 def get_hyperlink(url, text):
     return f"\033]8;;{url}\033\\{text}\033]8;;\033\\"
 
-load_dotenv()
 
+# Load environment variables
+load_dotenv()
 JIRA_SERVER = os.getenv('JIRA_SERVER')
 JIRA_USERNAME = os.getenv('JIRA_USERNAME')
 JIRA_API_TOKEN = os.getenv('JIRA_API_TOKEN')
 
-jira = JIRA(server=JIRA_SERVER, basic_auth=(JIRA_USERNAME, JIRA_API_TOKEN))
 
+# Log in to Jira
+jira = JIRA(server=JIRA_SERVER, basic_auth=(JIRA_USERNAME, JIRA_API_TOKEN))
 myself = jira.myself()
 print(f"Logged in as: {myself['displayName']} ({myself['emailAddress']})")
 
+
+# Query issues assigned to user
 jql_query = 'assignee = currentUser() ORDER BY priority DESC'
+issues = jira.search_issues(jql_query, maxResults=1000)
 
-issues = jira.search_issues(jql_query)
 
+# Order issues by status name
+status_order = {
+    "BACKLOG": 0,
+    "TO DO": 1,
+    "IN PROGRESS": 2,
+    "MERGED": 3,
+    "QA": 4,
+    "DONE": 5,
+    "REJECTED": 6
+}
+issues = sorted(issues, key=lambda issue: status_order.get(issue.fields.status.name.upper(), 100))
+
+
+# Pretty print result
 key_width = 15
 status_width = 15
 
